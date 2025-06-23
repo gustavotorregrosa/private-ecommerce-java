c9de8f9f916723ef0f00ffacf62321049aa4961c
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2025-06-23 09:15:37 -0300

    parcial

--- frontend/src/app/components/general-layout/general-layout.ts
+++ frontend/src/app/components/general-layout/general-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
 
@@ -8,6 +8,14 @@ import { Navbar } from '../navbar/navbar';
   templateUrl: './general-layout.html',
   styleUrl: './general-layout.scss'
 })
-export class GeneralLayout {
+export class GeneralLayout implements OnInit {
   
+  public theme: string = 'light';
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.theme = 'dark';
+    }, 3000);
+  }
+
 }
