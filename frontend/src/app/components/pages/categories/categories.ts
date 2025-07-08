0e2f519390186c752570b1bb35732322c4674bdc
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2025-07-08 20:41:04 -0300

    parcial

--- frontend/src/app/components/pages/categories/categories.ts
+++ frontend/src/app/components/pages/categories/categories.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../../services/httpService';
+import { ICategory } from '../../../interfaces/ICategory';
+import { IResponse } from '../../../interfaces/IResponse';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './categories.html',
   styleUrl: './categories.scss'
 })
 export class Categories implements OnInit {
 
-  public categories: any[] = [];
+  public categories: ICategory[] = [];
+  displayedColumns: string[] = ['name', 'actions'];
 
   constructor(private httpService: HttpService) {}
 
@@ -17,14 +23,18 @@ export class Categories implements OnInit {
     this.loadCategories();
   }
 
-  private loadCategories(): void {
-   this.httpService.get<any[]>('/categories')
-      .then(categories => {
-        this.categories = categories;
-      })
-      .catch(error => {
-        console.error('Error loading categories:', error);
-      })
+  private async loadCategories(): Promise<void> {
+    const _categories: ICategory[] = (await this.httpService.get<IResponse<ICategory[]>>('/categories')).data
+    this.categories = _categories;
+
+  }
+
+  public async doDelete(category: ICategory): Promise<void> {
+    console.log('Deleting category:', category);
+  }
+
+  public async doEdit(category: ICategory): Promise<void> {
+    console.log('Editing category:', category);
   }
 
 }
