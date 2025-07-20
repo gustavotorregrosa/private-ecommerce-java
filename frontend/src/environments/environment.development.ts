7e05bb3477b28830c6005afea403120441d8548e
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2025-07-20 16:08:17 -0300

    parcial

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,8 +1,13 @@
 export const development = {
     production: false,
-    apiUrl: 'http://localhost:8090',
-    socketUrl: 'http://localhost:8090/ws',
-    authUrl: 'http://localhost:3100/auth',
+    // apiUrl: 'http://localhost:8090',
+    // socketUrl: 'http://localhost:8090/ws',
+    // authUrl: 'http://localhost:3100/auth',
+
+    authUrl: 'https://dunder-mifflin.gustavo.torregrosa/auth',
+
+    apiUrl: 'https://api.dunder-mifflin.gustavo.torregrosa.dev',
+    socketUrl: 'https://api.dunder-mifflin.gustavo.torregrosa.dev/ws',
     version: '1.0.0-dev',
     featureFlags: {
         enableNewUI: true,
